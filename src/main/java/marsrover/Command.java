b0814ccd77c623f9326ee968b8bcaa31c78ab7bd
zxl0526 <851450219@qq.com>
2020-07-21 21:50:46 +0800

    Initialization engineering

---
+++ src/main/java/marsrover/Command.java
@@ -0,0 +1,15 @@
+package marsrover;
+
+public enum Command {
+    MOVE("M"), TURN_LEFT("L"), TURN_RIGHT("R");
+
+    private String shortName;
+
+    Command(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+}
